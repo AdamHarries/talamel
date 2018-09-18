be119caa93b672dd8502c6a17a2b2de143524088
Adam Harries <adam.harries@codeplay.com>
2018-09-18 18:17:11 +0100

    implement barbones functionality to make talamel useful!

--- talamel/apps/readmeta.c
+++ talamel/apps/readmeta.c
@@ -1,81 +1,43 @@
-/* Copyright (C) 2003 Scott Wheeler <wheeler@kde.org>
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions
- * are met:
- *
- * 1. Redistributions of source code must retain the above copyright
- *    notice, this list of conditions and the following disclaimer.
- * 2. Redistributions in binary form must reproduce the above copyright
- *    notice, this list of conditions and the following disclaimer in the
- *    documentation and/or other materials provided with the distribution.
- *
- * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
- * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
- * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
- * IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,
- * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
- * NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
- * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
- * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
- * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
- * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- */
-
 #include <stdio.h>
+#include <stdlib.h>
 #include <talamel.h>
 
-#ifndef FALSE
-#define FALSE 0
-#endif
+void print_metadata(const char* filename) { 
+  TalamelFile* f = tml_open_file(filename); 
 
-int main(int argc, char *argv[])
-{
-  int i;
-  int seconds;
-  int minutes;
-  TagLib_File *file;
-  TagLib_Tag *tag;
-  const TagLib_AudioProperties *properties;
+  char* title = tml_read_title(f); 
+  printf("Title: %s\n", title); 
+  free(title); 
 
-  taglib_set_strings_unicode(FALSE);
+  char* artist = tml_read_artist(f); 
+  printf("Artist: %s\n", artist); 
+  free(artist); 
 
-  for(i = 1; i < argc; i++) {
-    printf("******************** \"%s\" ********************\n", argv[i]);
+  int bpm = tml_read_bpm(f); 
+  printf("Bpm: %d\n", bpm);
 
-    file = taglib_file_new(argv[i]);
-
-    if(file == NULL)
-      break;
-
-    tag = taglib_file_tag(file);
-    properties = taglib_file_audioproperties(file);
-
-    if(tag != NULL) {
-      printf("-- TAG --\n");
-      printf("title   - \"%s\"\n", taglib_tag_title(tag));
-      printf("artist  - \"%s\"\n", taglib_tag_artist(tag));
-      printf("album   - \"%s\"\n", taglib_tag_album(tag));
-      printf("year    - \"%i\"\n", taglib_tag_year(tag));
-      printf("comment - \"%s\"\n", taglib_tag_comment(tag));
-      printf("track   - \"%i\"\n", taglib_tag_track(tag));
-      printf("genre   - \"%s\"\n", taglib_tag_genre(tag));
-    }
-
-    if(properties != NULL) {
-      seconds = taglib_audioproperties_length(properties) % 60;
-      minutes = (taglib_audioproperties_length(properties) - seconds) / 60;
-
-      printf("-- AUDIO --\n");
-      printf("bitrate     - %i\n", taglib_audioproperties_bitrate(properties));
-      printf("sample rate - %i\n", taglib_audioproperties_samplerate(properties));
-      printf("channels    - %i\n", taglib_audioproperties_channels(properties));
-      printf("length      - %i:%02i\n", minutes, seconds);
-    }
-
-    taglib_tag_free_strings();
-    taglib_file_free(file);
+  int comments = tml_count_comments(f); 
+  if(comments == 0)
+    printf("No comments found in file.\n");
+  for(int i = 0; i< comments; i++) { 
+    char * comm = tml_get_comment(f, i); 
+    printf("Comment [%d]: %s\n", i, comm); 
+    free(comm);
   }
+
+  tml_free_file(f); 
+}
+
+int main(int argc, char *argv[]) {
+  print_metadata("/home/adam/Music/Archive.org/Jumpin' at the "
+                    "Woodside - Count Basie And His Orchestra.mp3");
+  print_metadata(
+      "/home/adam/Music/iTunes/iTunes Media/Music/Michael Gamble/Michael "
+      "Gamble & the Rhythm Serenaders/19 Airmail Special.m4a");
+
+  print_metadata(
+    "/home/adam/Music/Dani Sicari & The Easy Rollers - Dani Sicari & The Easy Rollers/01 - Dani Sicari & The Easy Rollers - Some of these days.flac"
+  );
 
   return 0;
 }
