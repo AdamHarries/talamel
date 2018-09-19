cacf62028440627370c10877a9bafc9d95347168
Adam Harries <adam.harries@codeplay.com>
2018-09-19 18:15:28 +0100

    add main to examples, so it won't be compiled, but will be included in the repo

---
+++ examples/main.rs
@@ -0,0 +1,53 @@
+extern crate talamel;
+
+use std::path::PathBuf;
+
+use talamel::*;
+
+use std::env;
+
+use std::fmt::Debug;
+
+fn main() {
+    for arg in env::args().skip(1) {
+        print_metadata(arg);
+    }
+}
+
+fn print_metadata<P: Into<PathBuf> + Debug>(filename: P) -> () {
+    let f: TalamelFile = match TalamelFile::new(filename) {
+        Ok(f) => f, 
+        e => { 
+            println!("Got error {:?} while opening file!", e);
+            return;
+        }
+    };
+
+    match f.title() {
+        Ok(t) => println!("Title: {}", t),
+        e => println!("Could not read title, got error: {:?}", e),
+    }
+
+    match f.artist() {
+        Ok(a) => println!("Artist: {}", a),
+        e => println!("Could not read artist, got error: {:?}", e),
+    }
+
+    match f.read_property_values("ALBUM") {
+        Ok(vs) => {
+            for v in vs {
+                println!("Album: {}", v);
+            }
+        }
+        e => println!("Could not read album, got error: {:?}", e),
+    }
+
+    match f.comments() {
+        Ok(cs) => {
+            for comm in cs {
+                println!("Comment: {:?}", comm);
+            }
+        }
+        e => println!("Could not read comments, got error: {:?}", e),
+    }
+}
